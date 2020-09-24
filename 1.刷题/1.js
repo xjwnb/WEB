e9b385814ea804df0c042163b070c9f33a5fd709
1565163445@qq.com <1565163445@qq.com>
2020-09-24 19:32:38 +0800

    刷题总结

--- 1.刷题/1.js
+++ 1.刷题/1.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-24 17:38:35
- * @LastEditTime: 2020-09-24 19:03:59
+ * @LastEditTime: 2020-09-24 19:32:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\1.刷题\1.js
@@ -68,7 +68,7 @@ console.log(two.next().value); // a
     而在函数 generatorTwo 中， 我们通过 yeild* 关键字，就相当于一个迭代器，第一个 yield 的值就是 "a"。
 */
 
-// 题目 5：
+// 题目 6：
 function greeting() {
   throw "Hello World";
 }
@@ -86,4 +86,58 @@ sayHi(); // Oh no an error: Hello World
     由于函数 greeting 中使用 throw 创建了自定义错误，所以 try 中执行到该函数的时候就抛出错误，
     执行 catch 中的代码， 由于 greeting 中 throw 抛出的异常信息是 "Hello World"，所以 e 的值
     为 "Hello World"。
+*/
+
+// 题目 7：
+(() => {
+  let x = (y = 6);
+})();
+console.log(typeof x); // "undefined"
+console.log(typeof y); // "number"
+console.log(y); // 6
+/*
+原因：
+    let x = (y = 6) 这段代码是写在立即执行函数中的，x 是由 let 定义的，因此 x 的作用范围就只在
+    立即执行函数的代码块中，因此在外部是不能直接访问 x 的，因此 typeof x 的值是 "undefined"。
+    而 y 是直接赋值的。所以默认是用 var 定义 y 的没有块级作用域，所以外部能够访问。
+*/
+
+// 题目 8：
+const person = { name: "小卡车" };
+function say(age) {
+  console.log(`${this.name} is ${age}`);
+}
+say.call(person, 20); // 小卡车 is 20
+say.bind(person, 20); // function
+/*
+原因：
+    call 和 bind 两个函数都是可以改变函数的 this 指向，但是 call 是立即执行，
+    而 bind 函数则是返回函数的拷贝值，不会立即执行。
+*/
+
+// 题目 9:
+// module.js
+// export default () => "小卡车加油啊";
+// export const name = "小卡车";
+// index.js
+// import * as data from './module'
+// console.log(data); // { default: "小卡车加油啊", name: "小卡车" }
+/*
+原因：
+    使用 import * 。会将 module.js 中的所有 export 导入到 index.js 文件中。
+    而默认导出(export default)会有一个 default 的属性，而 export 导出则是对应的属性名。
+*/
+
+// 题目 10：
+const per = {
+  name: "小卡车",
+  age: 20,
+};
+for (const item in per) {
+  console.log(item); // "name" "age"
+}
+/*
+原因：
+    for in 是针对对象迭代的，item 是每次循环出来的 per 对象的键。
+    因此，item 是 "name" "age"。
 */
