cb9d08059e76ff3a1a69cb7bd4f8228bba10869e
1565163445@qq.com <1565163445@qq.com>
2020-09-30 20:14:20 +0800

    刷题

--- 01.刷题/07.js
+++ 01.刷题/07.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-09-30 19:59:59
- * @LastEditTime: 2020-09-30 20:06:52
+ * @LastEditTime: 2020-09-30 20:13:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\01.刷题\07.js
@@ -34,4 +34,23 @@ console.log(Object.keys(info)); // []
 /*
 原因：
     Symbol 类型是不可枚举的。Objec.keys 方法返回对象上的所有可枚举的键属性。Symbol 类型不可见，并返回一个空数组。
+*/
+
+// 题目 3：
+class Chameleon {
+  static colorChange(newColor) {
+    this.newColor = newColor;
+  }
+  constructor({ newColor = "green" } = {}) {
+    this.newColor = newColor;
+  }
+}
+const freddie = new Chameleon({ newColor: "purple" });
+console.log(freddie.newColor); // purple
+// freddie.colorChange("orange") // TypeError
+// Chameleon.colorChange("orange");
+// console.log(Chameleon.newColor); // orange
+/*
+原因：
+    Chameleon 类中 colorChange 是一个静态变量，因此不能通过实例进行调用，只能通过类名进行调用。
 */
