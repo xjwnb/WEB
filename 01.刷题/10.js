ca2c3d2fb41c03386b66dc4af2a53c2fef48e471
1565163445@qq.com <1565163445@qq.com>
2020-10-04 22:46:58 +0800

    update

--- 01.刷题/10.js
+++ 01.刷题/10.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-10-03 19:53:49
- * @LastEditTime: 2020-10-03 19:58:06
+ * @LastEditTime: 2020-10-03 20:01:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\01.刷题\10.js
  */
-//
+// 题目 1：
 class Dog {
   constructor(name) {
     this.name = name;
@@ -22,4 +22,19 @@ delete Dog.prototype.bark;
 /*
 原因：
     使用 delete 删除 Dog 类原型方法 bark，因此，后面又调用了，所以会抛出错误。TypeError: pet.bark is not a function 
+*/
+
+// 题目 2:
+function Person() {
+  this.name = "小卡车";
+  return {
+    name: "xkc",
+  };
+}
+const xkc = new Person();
+console.log(xkc); // { name: 'xkc' }
+console.log(xkc.name); // xkc
+/*
+原因：
+    Person 的实例拥有 Person 返回的对象。
 */
