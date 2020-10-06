0afeeb262e03906d0b2757a3e687c0841c4a7896
1565163445@qq.com <1565163445@qq.com>
2020-10-06 12:55:28 +0800

    update Set

--- 02.输出/07.Set.js
+++ 02.输出/07.Set.js
@@ -4,12 +4,23 @@ const set1 = new Set();
 set1.add("小卡车");
 set1.add(20);
 console.log(set1); // Set { '小卡车', 20 }
-// 判断是否有对应元素
+// has 判断是否有对应元素
 let hasEle = set1.has("小卡车");
 console.log(hasEle); // true
 // size 大小
 const size = set1.size;
 console.log(size); // 2
+// values 获得值
+const values = set1.values();
+console.log(values); // [Set Iterator] { '小卡车', 20 }
+for (let value of set1.values()) {
+  console.log(value); // 小卡车 20
+}
+// keys 获得键
+const keys = set1.keys();
+for (let key of set1.keys()) {
+  console.log(key); // 小卡车 20
+}
 // clear 删除所有值
 set1.clear();
 console.log(set1); // Set {}
