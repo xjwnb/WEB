ee794090e3f2792ad90af142a9051ea21518fd23
1565163445@qq.com <1565163445@qq.com>
2020-10-11 22:49:02 +0800

    update 期约

--- 02.输出/24.期约连锁与期约合成.js
+++ 02.输出/24.期约连锁与期约合成.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-11 20:15:47
- * @LastEditTime: 2020-10-11 21:48:28
+ * @LastEditTime: 2020-10-11 22:47:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\02.输出\24.期约连锁与期约合成.js
@@ -61,3 +61,39 @@ p1
 p2
 p3
 */
+
+// Promise.all()
+let p2 = Promise.all([Promise.resolve(1), Promise.resolve(2)]);
+setTimeout(console.log, 0, p2); // Promise { [ 1, 2 ] }
+p2.then(res => {
+  console.log("p2:", res); // p2: [ 1, 2 ]
+})
+
+// 会转换为 Promise.resolve()
+let p3 = Promise.all([1,2]);
+setTimeout(console.log, 0, p3); // Promise { [ 1, 2 ] }
+p3.then(res => {
+  console.log("p3:", res); // p3: [ 1, 2 ]
+})
+
+// 等价于 Promise.resolve()
+let p4 = Promise.all([]);
+setTimeout(console.log, 0, p4); // Promise { [] }
+p4.then(res => {
+  console.log("p4:", res); // p4: []
+});
+
+// 无效
+// let p5 = Promise.all(); // TypeError
+
+// 合成的期约要全部解决后才解决
+let p5 = Promise.all([
+  Promise.resolve("p5 - 1"),
+  new Promise((resolve) => {
+    resolve("p5 - 2");
+  }),
+]);
+setTimeout(console.log, 0, p5); // Promise { [ 'p5 - 1', 'p5 - 2' ] }
+p5.then(res => {
+  console.log("p5:", res); // p5: [ 'p5 - 1', 'p5 - 2' ]
+});
