9c2a912d8673a5593dee975be0c431c60b463312
1565163445@qq.com <1565163445@qq.com>
2020-10-11 09:40:42 +0800

    update 异步

--- 02.输出/22.异步.js
+++ 02.输出/22.异步.js
@@ -1,11 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-10-10 22:47:19
- * @LastEditTime: 2020-10-10 23:02:09
+ * @LastEditTime: 2020-10-11 09:35:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\02.输出\22.异步.js
  */
+
+// 异步返回值
 function test1(a, b, callback) {
   var a = a + b;
   // 异步操作
@@ -16,5 +18,42 @@ function test1(a, b, callback) {
 }
 // 传入数据并利用回调函数获得异步结果。
 test1(1, 2, (res) => {
-  console.log(res);
+  console.log(res); // 3
 });
+
+// 失败处理
+function double(value, success, failure) {
+  setTimeout(() => {
+    try {
+      if (
+        !(Object.prototype.toString.call(value) === "[object Object]") ||
+        !("name" in value)
+      ) {
+        throw "传入的数据有误！";
+      }
+      success(value["name"] + "加油！");
+    } catch (e) {
+      failure(e + "继续努力！");
+    }
+  });
+}
+double(
+  { name: "小卡车" },
+  (res) => {
+    console.log(`success: ${res}`);
+  },
+  (error) => {
+    console.log(`error: ${error}`);
+  }
+);
+// 输出 success: 小卡车加油！
+double(
+  {},
+  (res) => {
+    console.log(`success: ${res}`);
+  },
+  (error) => {
+    console.log(`error: ${error}`);
+  }
+);
+// 输出： error: 传入的数据有误！继续努力！
