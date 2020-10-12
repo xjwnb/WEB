4aa887aa99ab8f60894f01a57bae5ab29782b303
1565163445@qq.com <1565163445@qq.com>
2020-10-12 12:03:37 +0800

    update async

--- 02.输出/25.async.js
+++ 02.输出/25.async.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-12 11:33:03
- * @LastEditTime: 2020-10-12 11:40:34
+ * @LastEditTime: 2020-10-12 12:02:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\02.输出\25.async.js
@@ -19,7 +19,28 @@ async function foo2() {
 }
 foo2().then(console.log); // 1
 
+// 返回解决期约
 async function foo3() {
-  return Promise.resolve(1);
+  return Promise.resolve(2);
 }
-foo3().then(console.log); // 1
+foo3().then(console.log); // 2
+
+// 抛出错误会返回拒绝的期约
+async function foo4() {
+  throw 3;
+}
+foo4().catch(console.log); // 3
+
+// 返回拒绝期约
+async function foo5() {
+  return Promise.reject(4);
+}
+foo5().catch(console.log); // 4
+
+/*
+foo1
+1
+3
+2
+4
+*/
