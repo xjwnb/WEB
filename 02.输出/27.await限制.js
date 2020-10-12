37001410bd037d9ff43f02731e02980c1375cd7e
1565163445@qq.com <1565163445@qq.com>
2020-10-12 14:05:32 +0800

    update await

---
+++ 02.输出/27.await限制.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-12 13:51:45
+ * @LastEditTime: 2020-10-12 14:05:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \WEB\02.输出\27.await限制.js
+ */
+
+// await 必须在异步函数中使用。
+async function foo1() {
+  console.log(await Promise.resolve("foo1 - 1"));
+}
+foo1(); // "foo1 - 1"
+
+(async function foo2() {
+  console.log(await Promise.resolve("foo2 - 1"));
+})(); // foo2 - 1
+
+// await 必须存在在 async 的函数中，否则会报错。
+function foo3() {
+  const sync = async () => {
+    return await Promise.resolve("foo3 - 1");
+  };
+  sync().then(console.log); // foo3 - 1
+  console.log(sync()); // Promise { <pending> }
+}
+foo3();
