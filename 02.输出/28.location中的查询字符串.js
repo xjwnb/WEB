2124c9524fa47d045e56dcbd1c7aa166adacf0d0
1565163445@qq.com <1565163445@qq.com>
2020-10-13 16:09:03 +0800

    update location

--- 02.输出/28.location中的查询字符串.js
+++ 02.输出/28.location中的查询字符串.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-13 14:26:56
- * @LastEditTime: 2020-10-13 14:34:05
+ * @LastEditTime: 2020-10-13 15:38:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\02.输出\28.location中的查询字符串.js
@@ -20,3 +20,24 @@ let getQueryStringArgs = function () {
   }
   return args;
 };
+
+// URLSearchParams
+let qs = "?name=xkc&age=20";
+let searchParams = new URLSearchParams(qs);
+console.log(searchParams); // URLSearchParams { 'name' => 'xkc', 'age' => '20' }
+let hasName = searchParams.has("name");
+console.log(hasName); // true
+let name = searchParams.get("name");
+console.log(name); // xkc
+searchParams.set("hobby", "LOL");
+console.log(searchParams.toString()); // name=xkc&age=20&hobby=LOL
+searchParams.delete("hobby");
+console.log(searchParams.toString()); // name=xkc&age=20
+// 遍历
+for (let param of searchParams) {
+  console.log(param);
+}
+/*
+[ 'name', 'xkc' ]
+[ 'age', '20' ]
+*/
