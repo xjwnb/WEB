d4bab10192d988892d4f5e8bdf5e627425cb4985
1565163445@qq.com <1565163445@qq.com>
2020-10-20 12:17:22 +0800

    create&update javascript

---
+++ 07.JavaScript/test.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-20 12:14:34
+ * @LastEditTime: 2020-10-20 12:15:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \WEB\07.JavaScript\01.HTML中的JavaScript\test.js
+ */
+let xkcMsg = {
+  name: "小卡车",
+  age: 20,
+};
+let jsonText = JSON.stringify(xkcMsg);
+console.log(jsonText);
