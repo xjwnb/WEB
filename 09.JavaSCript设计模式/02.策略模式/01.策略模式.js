db365f74f9012d4457ecaf3db5c90b8a22d9fb63
1565163445@qq.com <1565163445@qq.com>
2020-10-22 19:57:55 +0800

    update 策略模式

--- 09.JavaSCript设计模式/02.策略模式/01.策略模式.js
+++ 09.JavaSCript设计模式/02.策略模式/01.策略模式.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-22 17:49:41
- * @LastEditTime: 2020-10-22 19:00:02
+ * @LastEditTime: 2020-10-22 19:57:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\09.JavaSCript设计模式\02.策略模式\01.策略模式.js
@@ -40,3 +40,54 @@ let calculateBonus1 = function (performancelevel, salary) {
   }
 };
 console.log(calculateBonus1("S", 20000)); // 80000
+
+// 3.使用策略模式重构代码
+let performanceS1 = function () {};
+performanceS1.prototype.calculate = function (salary) {
+  return salary * 4;
+};
+let performanceA1 = function () {};
+performanceA1.prototype.calculate = function (salary) {
+  return salary * 3;
+};
+let performanceB1 = function () {};
+performanceB1.prototype.calculate = function (salary) {
+  return salary * 2;
+};
+let Bonus = function () {
+  this.salary = null;
+  this.strategy = null;
+};
+Bonus.prototype.setSalary = function (salary) {
+  this.salary = salary;
+};
+Bonus.prototype.setStrategy = function (strategy) {
+  this.strategy = strategy;
+};
+Bonus.prototype.getBonus = function () {
+  if (!this.strategy) {
+    throw new Error("没有strategy属性");
+  }
+  return this.strategy.calculate(this.salary);
+};
+let bonus = new Bonus();
+bonus.setSalary(20000);
+bonus.setStrategy(new performanceS1());
+console.log(bonus.getBonus()); // 80000
+
+// JavaScript 版本的策略模式
+let strategies = {
+  S: function (salary) {
+    return salary * 4;
+  },
+  A: function (salary) {
+    return salary * 3;
+  },
+  B: function (salary) {
+    return salary * 2;
+  },
+};
+let calculateBonus2 = function (level, salary) {
+  return strategies[level](salary);
+};
+console.log(calculateBonus2("S", 20000)); // 80000
