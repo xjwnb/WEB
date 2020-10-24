f096e9564482147acff4cdf81fc0cb8b5120b1ec
1565163445@qq.com <1565163445@qq.com>
2020-10-24 21:21:58 +0800

    update 命令模式

--- 09.JavaSCript设计模式/06.命令模式/01.命令模式.js
+++ 09.JavaSCript设计模式/06.命令模式/01.命令模式.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-24 20:55:30
- * @LastEditTime: 2020-10-24 21:02:57
+ * @LastEditTime: 2020-10-24 21:21:53
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\09.JavaSCript设计模式\06.命令模式\01.命令模式.js
@@ -18,3 +18,32 @@ let button1 = document.createElement("button");
 button1.innerText = "执行刷新命令";
 document.body.appendChild(button1);
 bindClick(button1, MenuBar.refresh);
+
+
+let MoveCommand = {
+  run: function () {
+    console.log("奔跑");
+  },
+  stop: function () {
+    console.log("停止");
+  },
+  squatDown: function () {
+    console.log("蹲下");
+  },
+  walk: function () {
+    console.log("行走");
+  },
+};
+let triggerMove = function (dom, receiver, state) {
+  return dom.onclick = receiver[state];
+};
+let move = (function () {
+  let run = document.getElementById("run");
+  let stop = document.getElementById("stop");
+  let squatDown = document.getElementById("squatDown");
+  let walk = document.getElementById("walk");
+  triggerMove(run, MoveCommand, "run");
+  triggerMove(stop, MoveCommand, "stop");
+  triggerMove(squatDown, MoveCommand, "squatDown");
+  triggerMove(walk, MoveCommand, "walk");
+})();
