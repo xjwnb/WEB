0fabfa1587fb7ce8b58ddfd94dccae87affa7a28
1565163445@qq.com <1565163445@qq.com>
2021-07-05 23:49:47 +0800

    update

--- 100.面试/A.JavaScript/03.手写 bind、call、apply/01.bind.js
+++ 100.面试/A.JavaScript/03.手写 bind、call、apply/01.bind.js
@@ -1,18 +1,32 @@
 /*
  * @Author: your name
  * @Date: 2021-02-25 10:33:20
- * @LastEditTime: 2021-02-25 10:36:02
+ * @LastEditTime: 2021-07-04 22:55:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\100.面试\03.手写 bind、call、apply\01.bind.js
  */
 
-Function.prototype.mybind = function(ctx, ...args) {
+Function.prototype.mybind = function (ctx, ...args) {
   if (typeof this !== "function") {
     throw new TypeError("ERROR");
   }
   let _this = this;
   return function () {
     _this.apply(ctx, args);
+  };
+};
+
+// bind
+Function.prototype.mybind1 = function (newThis) {
+  let slice = Array.prototype.slice;
+  const args1 = slice.call(arguments, 1);
+  const fn = this;
+  if (typeof fn !== "function") {
+    throw new Error("目标非函数");
   }
-}
+  return function () {
+    const args2 = slice.call(arguments, 0);
+    return fn.apply(newThis, args1.concat(args2));
+  };
+};
--- 100.面试/A.JavaScript/06.拷贝/01.递归深拷贝.js
+++ 100.面试/A.JavaScript/06.拷贝/01.递归深拷贝.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-25 20:27:30
- * @LastEditTime: 2021-02-25 20:29:46
+ * @LastEditTime: 2021-07-04 22:43:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \WEB\100.面试\06.拷贝\01.递归深拷贝.js
@@ -24,3 +24,68 @@ function deepCopy(obj) {
   }
   return res;
 }
+
+/**
+ * 高级拷贝
+ */
+class DeepClone {
+  constructor () {
+    this.cacheList = [];
+  }
+
+  clone (source) {
+    if (source instanceof Object) {
+      const cache = this.findCache(source);
+      if (cache) {
+        return cache;
+      } else {
+        let target = {};
+        if (source instanceof Array) {
+          target = new Array();
+        } else if (source instanceof Function) {
+          target = function () {
+            return source.apply(this, arguments);
+          }
+        } else if (source instanceof Date) {
+          target = new Date(source);
+        } else if (source instanceof RegExp) {
+          target = new RegExp(source.source, source.flags);
+        }
+        this.cacheList.push([source, target]);
+        for (let key in source) {
+          if (source.hasOwnProperty(key)) {
+            target[key] = this.clone(source[key]);
+          }
+        }
+        return target;
+      }
+    }
+    return source; 
+  }
+
+  findCache (source) {
+    for (let i = 0; i < this.cacheList.length; i++) {
+      if (this.cacheList[i][0] == source) {
+        return this.cacheList[i][1];
+      }
+    }
+    return undefined;
+  }
+}
+
+const obj = {
+  date: new Date(),
+  name: "小卡车",
+  reg: /^[0-9]$/g,
+  info: {
+    habby: ["打游戏", "发呆"],
+    age: 21,
+  },
+  getName: function () {
+    return this.name;
+  },
+};
+
+const deepClone = new DeepClone();
+const objCopy = deepClone.clone(obj);
+console.log(objCopy);
